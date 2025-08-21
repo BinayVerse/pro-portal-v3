091bf82b8d5e72922c67e22fc283b66da1d9b360
Binay <binaykumarp@proximabiz.com>
2025-08-22 02:39:29 +0530

    initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+// Initialize auth on app start
+const authStore = useAuthStore()
+onMounted(() => {
+  authStore.initializeAuth()
+})
+</script>
