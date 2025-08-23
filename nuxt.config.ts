d43745862b86c910623861a6b93fc93923aa8424
Binay <binaykumarp@proximabiz.com>
2025-08-23 13:16:22 +0530

    modified config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -119,9 +119,9 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
-          hid: 'description',
-          name: 'description',
-          content: 'A modern Vue 3 dashboard with Nuxt UI',
+          hid: 'provento.ai',
+          name: 'provento.ai',
+          content: 'Your AI Partner for Smarter Interactions',
         },
       ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
